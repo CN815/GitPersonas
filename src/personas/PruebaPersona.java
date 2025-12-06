678051796907a3511a2a2eb15f1af7f001149145
CN815 <cristina.xu4@educa.madrid.org>
2025-12-06 21:28:38 +0100

    Update PruebaPersona.java

--- src/personas/PruebaPersona.java
+++ src/personas/PruebaPersona.java
@@ -17,7 +17,7 @@ public class PruebaPersona {
          // Creamos varias instancias de personas
          Persona per1 = new Persona("20222444L", "María", "Carcelén Sánchez", 40);
          Persona per2 = new Persona("José", "Serrano Márquez", 25);
-         Persona per3 = new Persona("64578488K","Pedro", "De la Calle García", 55);
+         Persona per3 = new Persona("64578488K","Carlos", "De la Calle García", 55);
          
          //Almacenamos las instancias de personas en un array
          Persona[] arrayPersonas = new Persona[3];
